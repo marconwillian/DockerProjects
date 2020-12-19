d34b2429ab26449f239803ac3fd9138b990f6b75
Marcon Willian <marconwillian@protonmail.com>
2020-12-19 10:17:25 -0300

    feat: Add redeploy

---
+++ 3000 - Ecoleta_Server/redeploy.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+
+docker images | awk '(NR>1) && ($2!~/none/) {print $1":"$2}' | xargs -L1 docker pull
+docker-compose pull
+docker-compose up -d
+docker system prune --all --force --volumes
